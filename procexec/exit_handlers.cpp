4f9bf0d1da8269da5d370a1112bb5ea127273b80
siukwanCentOS <pypy39353@qq.com>
2016-01-18 13:27:21 +0800

    退出处理程序实验

---
+++ procexec/exit_handlers.cpp
@@ -0,0 +1,39 @@
+/*
+ * 使用退出处理程序
+ * */
+#define _BSD_SOURCE
+#include <stdlib.h> //on_exit()函数在<stdlib.h>中
+#include "../lib/tlpi_hdr.h"
+
+static void
+atexitFunc1(void)
+{//无参数
+	printf("atexit function 1 called\n");
+}
+
+static void
+atexitFunc2(void)
+{
+	printf("atexit function 2 called\n");
+}
+
+static void
+onexitFunc(int exitStatus, void *arg)
+{
+	printf("on_exit function called: status = %d, arg=%ld\n",exitStatus, (long) arg);
+}
+
+int
+main(int argc, char *argv[])
+{
+	if(on_exit(onexitFunc,(void*)10)!=0)
+		fatal("on_exit 1");
+	if(atexit(atexitFunc1)!=0)
+		fatal("atexit 1");
+	if(atexit(atexitFunc2)!=0)
+		fatal("atexit 2");
+	if(on_exit(onexitFunc, (void*)20)!=0)
+		fatal("on_exit 2");
+
+	exit(2);
+}
